e984c686559f7a1142576720dcfa95534ec72428
EliNAyli <elicuro96@gmail.com>
2022-10-31 18:36:36 -0500

    rama1 add 4

--- archiv1.py
+++ archiv1.py
@@ -8,6 +8,9 @@ def function2():
     print("esto es una funcion 2")
 
 def function3():
-    print("esto es una funcion 2")
+    print("esto es una funcion 3")
+
+def function4():
+    print("esto es una funcion 4")
 
     Pass
